ae86fd95cb01db50699c2cf87c1762b1f68bf3f9
Madhumanoj3 <madhumanoj094@gmail.com>
2026-08-05 15:56:47 +0530

    Refactor error handling in BitStuffing.c

--- Framing Protocols/BitStuffing.c
+++ Framing Protocols/BitStuffing.c
@@ -52,42 +52,44 @@ int main()
 
     printf("\nDo you want to induce an error? (Y/N) : ");
     scanf(" %c", &ch);
+    if (ch=='N'||ch=='n') {
+       printf("Destuffed data: %s\n",stuffed);
+    }
 
     if(ch == 'Y' || ch == 'y')
     {
         len = strlen(stuffed);
-printf("Choose Error Type:\n1. Single-bit error\n2. Multiple-bit error\nEnter choice (1 or 2): ");        scanf("%d", &error_type);
+        printf("Choose Error Type:\n1. Single-bit error\n2. Multiple-bit error\nEnter choice (1 or 2): ");        scanf("%d", &error_type);
 
        if(error_type == 1)
-{
-    printf("Enter Bit Position (1-%d) : ", len);
-    scanf("%d", &pos);
+       {
+          printf("Enter Bit Position (1-%d) : ", len);
+          scanf("%d", &pos);
 
-    if(pos >= 1 && pos <= len)
-    {
-        stuffed[pos - 1] = (stuffed[pos - 1] == '0') ? '1' : '0';
-        printf("\nBit at Position %d flipped.", pos);
-        printf("\n\nFrame After Single-Bit Error : %s", stuffed);
-        printf("\nTransmission Error Detected! Frame Discarded.");
-    }
-    else
-    {
-        printf("\nInvalid Bit Position.\n");
-    }
-}
-else if(error_type == 2)
-{
-    int numBits, positions[100];
+          if(pos >= 1 && pos <= len)
+          {
+             stuffed[pos - 1] = (stuffed[pos - 1] == '0') ? '1' : '0';
+             printf("\nBit at Position %d flipped.", pos);
+             printf("\n\nFrame After Single-Bit Error : %s", stuffed);
+             printf("\nTransmission Error Detected! Frame Discarded.");
+        }
+          else
+          {
+             printf("\nInvalid Bit Position.\n");
+          }
+       }
+       else if(error_type == 2)
+       {
+        int numBits, positions[100];
+        printf("Enter Number of Bits to Flip : ");
+        scanf("%d", &numBits);
 
-    printf("Enter Number of Bits to Flip : ");
-    scanf("%d", &numBits);
-
-    if(numBits <= 0 || numBits > len)
-    {
+        if(numBits <= 0 || numBits > len)
+        {
         printf("\nInvalid Number of Bits.\n");
-    }
-    else
-    {
+        }
+       else
+       {
         for(i = 0; i < numBits; i++)
         {
             printf("Enter Bit Position %d (1-%d) : ", i + 1, len);
@@ -109,7 +111,8 @@ else if(error_type == 2)
         printf("\nTransmission Error Detected! Frame Discarded.");
     }
 }
-    else
+
+else
     {
         printf("\nNo Error Introduced.");
         count = 0;
@@ -135,4 +138,4 @@ else if(error_type == 2)
         printf("\n\nReceiver Output : %s\n", destuffed);
     }
     return 0;
-}
+} }
